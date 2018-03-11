258a1d3728273feee13c193ebfc0d368794dfd93
İbrahim Bağrıyanık <ibrahim.bagriyanik@hotmail.com>
2018-03-11 11:25:58 +0300

    Temel HTML kodları ve dosya hiyerarşisi.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="title" content="Minether | Minecraft modları, haritaları ve texturepackleri.">
+		<meta name="description" content="Minether.com'da en güncel Minecraft modları, Minecraft haritaları ve Minecraft texturepacklerini bulabilirsiniz.">
+		<meta name="keywords" content="minecraft, minecraft indir, minecraft modları, minecraft texturepackleri, minecraft haritaları, minecraft kaynak paketleri, miecraft 1.8, minecraft oyna">
+		<meta name="robots" content="index, follow">
+		<meta name="language" content="Turkish">
+		<meta name="author" content="İbrahim Bağrıyanık">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/content.css">
+	</head>
+
+	<body>
+		<div id="header"></div>
+		<div id="content"></div>
+	</body>
+</html>
